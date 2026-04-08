2aeec0f52848a27b5cdcf58ea46ae6fc2448762d
cmboyd-sudo <cmboyd@economyelectricalservices.com>
2026-04-08 15:16:56 -0400

    Add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          500: '#f97316',
+          600: '#ea580c',
+        },
+        pink: {
+          500: '#ec4899',
+          600: '#db2777',
+        },
+      },
+      animation: {
+        'gradient': 'gradient 8s linear infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
